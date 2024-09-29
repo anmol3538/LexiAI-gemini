03c8427109883429e6a69f08b726728242927afe
anmol3538 <anmolaggarwal557@gmail.com>
2024-09-29 16:49:08 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './components/sidebar/s'
+import Main from './components/main/main'
+const App = () => {
+  return (
+    <>
+        <Sidebar/>
+        <Main/>
+    </>
+  )
+}
+
+export default App
